742005a7d5f671d3cd1d0f557f2eeed6237b401e
wnrome21 <nateromero001@gmail.com>
2022-03-29 17:36:38 -0700

    split components into muliple components

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ExpenseDate from './ExpenseDate';
+import './ExpenseItem.css';
+
+const ExpenseItem = (props) => {
+
+ 
+
+    return ( 
+        <div className="expense-item">
+            <ExpenseDate 
+            date={props.date}
+            title={props.title}
+            amount={props.amount}
+            />
+            <div className="expense-item__description">
+                <h2>{props.title}</h2>
+                <div className="expense-item__price">${props.amount}</div>
+            </div>
+        </div>
+     );
+}
+ 
+export default ExpenseItem;
