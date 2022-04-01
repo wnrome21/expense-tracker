e3b4d4b110b2f583a4acfeb743a4b0536ea070e1
wnrome21 <nateromero001@gmail.com>
2022-03-31 17:31:39 -0700

    component functions execution

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ExpenseDate from './ExpenseDate';
-import Card from './Card';
+import Card from '../UI/Card';
 import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
-
+    const clickHandler = () => {
+        console.log('Clicked!')
+    }
  
 
     return ( 
@@ -16,6 +18,7 @@ const ExpenseItem = (props) => {
                 <h2>{props.title}</h2>
                 <div className="expense-item__price">${props.amount}</div>
             </div>
+            <button onClick={clickHandler}>Change Title</button>
         </Card>
      );
 }
