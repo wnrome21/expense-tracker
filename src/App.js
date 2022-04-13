7c870d859ddb110703a455ea4d90a882eeea08a4
wnrome21 <nateromero001@gmail.com>
2022-04-12 23:34:08 -0700

    added keys to each rendered expense item and conducted overhaul on color palette for app

--- src/App.js
+++ src/App.js
@@ -1,38 +1,43 @@
+import React, { useState } from 'react';
 import './App.css';
 import Expenses from './components/Expenses/Expenses';
 import NewExpense from './components/NewExpense/NewExpense';
 
 
+const Static_Expenses = [
+    {
+      id: 'e1',
+      title: 'Toilet Paper',
+      amount: 94.12,
+      date: new Date(2020, 7, 14)
+    },
+    {
+      id: 'e2',
+      title: 'New Tv',
+      amount: 799.49,
+      date: new Date(2021, 2, 12)
+    },
+    {
+      id: 'e3',
+      title: 'Car Insurance',
+      amount: 294.67,
+      date: new Date(2021, 2, 28)
+    },
+    {
+      id: 'e4',
+      title: 'New Desk (Wooden)',
+      amount: 450,
+      date: new Date(2021, 5, 12)
+    }
+  ]
+
 const App = () => {
-  const expenses = [
-  {
-    id: 'e1',
-    title: 'Toilet Paper',
-    amount: 94.12,
-    date: new Date(2020, 7, 14)
-  },
-  {
-    id: 'e2',
-    title: 'New Tv',
-    amount: 799.49,
-    date: new Date(2021, 2, 12)
-  },
-  {
-    id: 'e3',
-    title: 'Car Insurance',
-    amount: 294.67,
-    date: new Date(2021, 2, 28)
-  },
-  {
-    id: 'e4',
-    title: 'New Desk (Wooden)',
-    amount: 450,
-    date: new Date(2021, 5, 12)
-  }
-]
+  const [expenses, setExpenses] = useState(Static_Expenses)
 
 const addExpenseHandler = (expense) => {
-  console.log(expense);
+  setExpenses(prevExpenses => {
+    return [expense, ...prevExpenses]
+  });
 }
 
   return (
--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,16 +1,9 @@
-import React, { useState } from 'react';
+import React from 'react';
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
-    const [title, setTitle] = useState(props.title);
-
-    const clickHandler = () => {
-        setTitle('Updated!');
-        console.log(title);
-    }
- 
 
     return ( 
         <Card className="expense-item">
@@ -18,10 +11,9 @@ const ExpenseItem = (props) => {
             date={props.date}
             />
             <div className="expense-item__description">
-                <h2>{title}</h2>
+                <h2>{props.title}</h2>
                 <div className="expense-item__price">${props.amount}</div>
             </div>
-            <button onClick={clickHandler}>Change Title</button>
         </Card>
      );
 }
